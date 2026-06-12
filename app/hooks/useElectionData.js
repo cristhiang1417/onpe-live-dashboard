2009194835a37a23e0d8b8e20e34b857a4fc2438
Cristhian Gomez <cristhian.gomez@kyndryl.com>
2026-06-12 10:56:27 -0500

    Sprint 6 - Actualizacion automatica de resultados

--- app/hooks/useElectionData.js
+++ app/hooks/useElectionData.js
@@ -1,23 +1,44 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import api from "../services/api";
 import { electionData } from "../data/mockData";
 
 export default function useElectionData() {
   const [data, setData] = useState(electionData);
 
   useEffect(() => {
-    async function load() {
-      try {
-        const res = await api.get("/results");
-        setData(res.data);
-      } catch (e) {
-        console.log("Usando datos mock");
-      }
-    }
+    const interval = setInterval(() => {
+      setData((prev) => {
+        const leftVotes = prev.left.votes + Math.floor(Math.random() * 120);
+        const rightVotes = prev.right.votes + Math.floor(Math.random() * 150);
 
-    load();
+        const totalVotes = leftVotes + rightVotes;
+
+        const leftPercent = Number(((leftVotes / totalVotes) * 100).toFixed(3));
+        const rightPercent = Number(((rightVotes / totalVotes) * 100).toFixed(3));
+
+        return {
+          ...prev,
+          updatedAt: new Date().toLocaleTimeString("es-PE"),
+
+          left: {
+            ...prev.left,
+            votes: leftVotes,
+            percent: leftPercent,
+          },
+
+          right: {
+            ...prev.right,
+            votes: rightVotes,
+            percent: rightPercent,
+          },
+
+          difference: Math.abs(rightVotes - leftVotes),
+        };
+      });
+    }, 5000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return data;
